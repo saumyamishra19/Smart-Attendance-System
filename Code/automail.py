087c0ca33f65c824e961aab326531fd822b6aa3a
Saumya Mishra <156930840+saumyamishra19@users.noreply.github.com>
2025-01-30 23:26:35 +0530

    Update automail.py

--- Code/automail.py
+++ Code/automail.py
@@ -3,7 +3,7 @@ import os
 
 receiver = "mygmail@gmail.com"  # receiver email address
 body = "Attendence File"  # email body
-filename = "Attendance"+os.sep+"Attendance_2020-08-29_17-05-04.csv"
+filename = "Attendance"+os.sep+"Attendance_2025-01-09_17-05-04.csv"
 
 yag = yagmail.SMTP("mymail@gmail.com", "my_password")
 
